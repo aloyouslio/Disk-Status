e1f94ea8f7673062746a54cb84ae8ee9691f3948
aloyous <35898246+aloyouslio@users.noreply.github.com>
2018-11-13 13:03:57 +0800

    Update maildisk.sh

--- maildisk.sh
+++ maildisk.sh
@@ -2,4 +2,4 @@
 LANG="en_US.UTF8" ; export LANG
 d=`date +%y%m%d%H%M%S`
 data=$( cat /root/disk/status.txt |  tr -d \\r)
-cat /root/disk/status.txt |  tr -d \\r |  /root/disk/mail.sh  notification@gmail.com "Daily disk status report "$d "$data"
+/root/disk/mail.sh  notification@gmail.com "Daily disk status report "$d "$data"
